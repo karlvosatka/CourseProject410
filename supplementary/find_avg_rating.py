ab8b3f6c70c414d4eb4aade1bf479dcc53eb89f7
Karl Vosatka <86745145+karlvosatka@users.noreply.github.com>
2022-12-06 09:42:59 -0600

    Update find_avg_rating.py

--- supplementary/find_avg_rating.py
+++ supplementary/find_avg_rating.py
@@ -1,3 +1,6 @@
+#the "ratings_export.csv" file referenced below is from a kaggle dataset of letterboxd reviews site wide. This file calculates the average rating sitewide for each movie,
+#which we reference as the cutoff point for labelling a rating score as positive of negative throughout the software.
+#Kaggle link: https://www.kaggle.com/datasets/samlearner/letterboxd-movie-ratings-data?select=ratings_export.csv
 import pandas as pd
 
 ratings = pd.read_csv("ratings_export.csv")
